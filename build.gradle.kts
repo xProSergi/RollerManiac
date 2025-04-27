399ea40b2405d823a6aae0b40bca2e1104f3b228
Sergio <sergioinstituto21.22@gmail.com>
2025-04-27 11:35:11 -0500

    Implementacion perfil

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal() // Asegúrate de tener este repositorio
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.0") // Reemplaza con la versión más reciente o recomendada
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.10") // Asegúrate de que la versión coincida con settings.gradle.kts
+        classpath("com.google.gms:google-services:4.4.1") // Asegúrate de tener esta línea si usas Firebase
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
